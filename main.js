d92bf21030513bf5832014e0a250155896d2e372
Fan4ik1308 <moonaraves@gmail.com>
2025-02-05 21:29:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let String1 = prompt("String typedagi malumot kriting");
+let Number1 = prompt("Number typedagi malumot kriting");
+let Boolean1 = prompt("Boolean typedagi malumot kriting");
+let Null = prompt("Null typedagi malumot kiriting");
+confirm("hamma malumotlarni to'g'ri kiritganizga ishonch komolmi?");
+alert(`Siz kiritgan ${String1} sozi string typega tegishli 
+Siz kiritgan ${Number1} sozi number typega tegishli
+Siz kiritgan ${Boolean1} sozi boolean typega tegishli
+Siz kiritgan ${Null} sozi null typega tegishli`)   
+
